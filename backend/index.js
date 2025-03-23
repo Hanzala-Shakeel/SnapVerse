0f912a4c290ae7e20d220213799a3cc3754b0c85
unknown <hubaishhuman389@gmail.com>
2025-03-24 04:50:07 +0500

    some changes

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ console.log(___dirname);
 app.use(express.json());
 
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true // Allow credentials (cookies) to be included
 }));
 
